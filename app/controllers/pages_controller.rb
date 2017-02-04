9490019ff92538d42c1892c2758062460fe8ef52
Mi Hi <miika_hirvasmaa@hotmail.com>
2017-02-04 21:08:45 +0000

    Refactoringa

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,16 @@ class PagesController < ApplicationController
     def home
     end
     
+    def gallery
+    end
+    
     def about
+    end
+    
+    def blog
+    end
+    
+    
+    def contact
     end
 end
